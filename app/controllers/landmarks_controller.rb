3c18b7e58614e16b116d46b62ccb447d8325bfc7
Seb Gertz <sebgertz@gmail.com>
2017-10-16 06:16:24 +0000

    One hopeless mess

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,29 @@
 class LandmarksController < ApplicationController
 
-end
+   get '/landmarks/new' do
+     erb :'landmarks/new'
+   end
+
+   get '/landmarks' do
+     @landmarks = Landmark.all
+     erb :'landmarks/index'
+   end
+
+   get '/landmarks/:id' do
+     @landmark = Landmark.find_by_id(params[:id])
+     erb :'landmarks/show'
+   end
+
+   get '/landmarks/:id/edit' do
+     @landmark = Landmark.find_by_id(params[:id])
+     erb :'/landmarks/edit'
+   end
+
+   post '/landmarks/:id' do
+     @landmark = Landmark.find_by_id(params[:id])
+     @landmark.update(params[:landmark])
+
+     redirect "/landmarks/#{@landmark.id}"
+   end
+
+end		
